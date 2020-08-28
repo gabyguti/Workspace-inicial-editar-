889707833a5d2e52f995531ced8c026163cbe5b3
gabyguti <gabygf92@gmail.com>
2020-08-28 00:33:57 -0300

    Modificaciones (prueba)

--- js/products.js
+++ js/products.js
@@ -33,11 +33,12 @@
     //Función que se ejecuta una vez que se haya lanzado el evento de
     //que el documento se encuentra cargado, es decir, se encuentran todos los
     //elementos HTML presentes.
+
     document.addEventListener("DOMContentLoaded", function(e) {
         getJSONData(PRODUCTS_URL).then(function(resultObj) {
             if (resultObj.status === "ok") {
                 productsArray = resultObj.data;
-                //Muestro las categorías ordenadas
+                //Muestro las categorías ordenadas HJFHF
                 showProductsList(productsArray);
             }
         });
