8eca1fae9d741f9c6deab688079e9ac59067d5c2
gabyguti <gabygf92@gmail.com>
2020-09-09 15:42:05 -0300

    9/9

--- js/product-info.js
+++ js/product-info.js
@@ -11,9 +11,26 @@ function showImages(array) {
                 <div class="d-block mb-4 h-100">
                     <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
                 </div>
-            </div>q
+            </div>
             `
         document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
+    }
+}
+
+function showImages2(array) {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+        let image = array[i];
+
+        htmlContentToAppend += `
+            <div class="col-lg-3 col-md-4 col-6">
+                <div class="d-block mb-4 h-100">
+                    <img class="img-fluid img-thumbnail" src="` + image + `" alt="">
+                </div>
+            </div>
+            `
+        document.getElementById("productImagesGallery2").innerHTML = htmlContentToAppend;
     }
 }
 
@@ -24,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
     getJSONData(PRODUCT_INFO_URL).then(function(resultObj) {
         if (resultObj.status === "ok") {
             carArray = resultObj.data;
-
+            //Datos del producto
             let a = document.getElementById("categoryName");
             let b = document.getElementById("categoryDescription");
             let c = document.getElementById("cost");
@@ -40,6 +57,25 @@ document.addEventListener("DOMContentLoaded", function(e) {
             f.innerHTML = carArray.category;
             //Muestro las imagenes en forma de galería
             showImages(carArray.images);
+            showImages2(carArray.relatedProducts);
         }
     });
+});
+
+//Comentarios del producto
+comentario = document.getElementById("addComment"); //textarea
+error = document.getElementById("textError"); // mensaje error
+submit = document.getElementById("submitComment"); //botón "Enviar"
+exit = document.getElementById("commentExit"); // mensaje comentario exitoso
+
+submit.addEventListener("click", () => {
+    if (comentario.value == "" || comentario.value == null) {
+        comentario.classList.add("bordeRojo");
+        error.style.display = "block";
+    } else {
+        exit.style.display = "block";
+        comentario.classList.add("bordeVerde");
+        comentario.value = "";
+        error.style.display = "none";
+    }
 });
