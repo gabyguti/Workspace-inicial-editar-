9a6957578486f9b77ab491969c4d88c636f112db
gabyguti <gabygf92@gmail.com>
2020-10-09 03:17:45 -0300

    Json carrito

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,10 @@
-/*var cartArray = [];*/
 const CART_URL = "https://japdevdep.github.io/ecommerce-api/cart/654.json";
 let jsonGeneral;
+let jsonGeneral1;
+let jsonSubtotal;
+let jsonTotal;
 
+/*Cálculo cantidad de productos por costo unitario del Producto 1*/
 function result(jsonGeneral) {
     let costo;
     let object = jsonGeneral.articles;
@@ -17,16 +20,51 @@ function cartt() {
     result(jsonGeneral);
 }
 
+/*Mismo cálculo del Producto 2*/
+function result1(jsonGeneral1) {
+    let costo;
+    let object = jsonGeneral1.articles;
+    let valor = document.getElementById('selector1').value;
+    if (object[1].currency == 'USD') {
+        costo = object[1].unitCost;
+    }
+    let resultado = valor * costo;
+    document.getElementById('carro1').innerHTML = object[1].currency + ` ` + resultado + ``;
+}
+
+function cartt1() {
+    result1(jsonGeneral1);
+}
+
+/*Cálculo subtotal*/
+function subtotal(jsonSubtotal) {
+    let valorProducto1 = document.getElementById('carro').value;
+    let valorProducto2 = document.getElementById('carro1').value;
+    let object = ss.articles;
+    let resultado = valorProducto1 + valorProducto2;
+    document.getElementById('subtotal').innerHTML = object[1].currency + ` ` + resultado + ``;
+}
+
+function cartt2() {
+    subtotal(jsonSubtotal);
+}
+
+
 document.addEventListener("DOMContentLoaded", function(e) {
     getJSONData(CART_URL).then(function(resultObj) {
         if (resultObj.status === "ok") {
             console.log(resultObj.data);
             jsonGeneral = resultObj.data;
-        }
+            jsonGeneral1 = resultObj.data;
+            jsonSubtotal = resultObj.data;
+        };
     });
 });
 
-/*
+
+// Json productos del carrito
+var cartArray = [];
+
 function showCart(array) {
     let htmlContentToAppend = "";
     for (let i = 0; i < array.length; i++) {
@@ -34,34 +72,28 @@ function showCart(array) {
 
         htmlContentToAppend += `
         <a href="cart.html" class="list-group-item list-group-item-action">
-                 <div class="col-3">
-                <h5 class="mb-1">` + cart.articles.name + `</h5>
+                 <div class="row">
+                <h5 class="col-sm">` + cart.name + `</h5>
                 </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h7 class="font-weight-bold mb-1">` + cart.articles.count + `</h7>
-                        <small>` + cart.articles.unitCost + " " + "<br>" + `</small>
-                    </div>
-                    <h6 class="mb-1">` + cart.articles.currency + `</h6>
-                    <div class="col-2">
-                        <img src="` + cart.articles.imgSrc + `" alt="` + `" class="img-thumbnail">
+                <div> 
+                    <h6 class="mb-1">` + cart.currency + " " + cart.unitCost + `</h6>
+                    <h7 class="font-weight-bold mb-1">` + cart.count + `</h7>
+                    <div class="col-4">
+                        <img src="` + cart.src + `" alt="` + `" class="img-thumbnail">
                     </div>
                 </div>
             </div>
         </a>
         `
-        document.getElementById("cart").innerHTML = htmlContentToAppend;
     }
-}
-*/
+    document.getElementById("cart").innerHTML = htmlContentToAppend;
+};
 
-
-/*
 document.addEventListener("DOMContentLoaded", function(e) {
-    getJSONData(CART_INFO_URL).then(function(resultObj) {
+    getJSONData(CART_URL).then(function(resultObj) {
         if (resultObj.status === "ok") {
             cartArray = resultObj.data;
-            showCart(cartArray);
-        }
+            showCart(cartArray.articles);
+        };
     });
-}); */
+});
